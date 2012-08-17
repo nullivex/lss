0ad3f9acb76564fe9a361ef266687b9d5b8c3f53
root <root@dev.openlss.com>
2012-08-17 16:01:28 -0700

    initial core commit

---
+++ ctl/admin/member_create.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * LSS Core
+ * OpenLSS - Light, sturdy, stupid simple
+ * 2010 Nullivex LLC, All Rights Reserved.
+ * Bryan Tong <contact@nullivex.com>
+ *
+ *   OpenLSS is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   OpenLSS is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with OpenLSS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once(ROOT.'/lib/members.php');
+
+page_header();
+
+if(post('create')){
+	try {
+		Members::_get()->create(post());
+		alert('member added successfully',true,true);
+		redirect(Url::members());
+	} catch (Exception $e){
+		alert($e->getMessage(),false);
+	}
+}
+
+$params = Members::_get()->createParams();
+$params = array_merge($params,post());
+$params['is_affiliate'] = post('is_affiliate') ? 'checked="checked"' : '';
+Tpl::_get()->parse('members','create',$params);
+
+page_footer();
+output(Tpl::_get()->output());
