419004b11103e5f4c94e64e56111477053f89fbe
root <root@dev.openlss.com>
2012-08-17 16:03:16 -0700

    removed old core

--- bin/build.php
+++ bin/build.php
@@ -10,7 +10,7 @@ require_once('bin/lib/phpkg.php');
 
 //figure out our opts
 $so = 'crv';
-$lo = array('core','rebase','verbose','seeds:','leafs:','out:','tree:');
+$lo = array('core','rebase','verbose','seeds:','roots:','leafs:','out:','tree:');
 $opts = getopt($so,$lo); unset($so,$lo);
 
 //check if we are supposed to be verbose or even quiet
@@ -40,36 +40,55 @@ if(!is_null(gfa($opts,'core')) || !is_null(gfa($opts,'c'))){
 if(gfa($opts,'seeds')){
 	dolog('starting to install seeds');
 	foreach(explode(',',gfa($opts,'seeds')) as $seed){
-		$seed_path = ROOT.'/seed/'.$seed.'/';
+		$seed_path = '/seed/'.$seed.'/';
 		//check if seed exists
-		if(!is_dir($seed_path)){
+		if(!is_dir(ROOT.$seed_path)){
 			dolog('WARNING: seed '.$seed.' does not exist');
 			continue;
 		}
 		//clone in seed contents
 		dolog('install seed '.$seed);
-		$manifest = readManifest($seed_path);
+		$manifest = readManifest(ROOT.$seed_path);
 		foreach($manifest as $mani_item)
-			run('cp -av '.$seed_path.$mani_item.' '.$dest);
+			run('cp -av '.ROOT.$seed_path.$mani_item.' '.$dest.$seed_path.$mani_item);
 	}
 	dolog('done installing seeds');
+}
+
+//copy the roots
+if(gfa($opts,'roots')){
+	dolog('starting to install roots');
+	foreach(explode(',',gfa($opts,'roots')) as $root){
+		$root_path = '/root/'.$root.'/';
+		//check if seed exists
+		if(!is_dir(ROOT.$root_path)){
+			dolog('WARNING: root '.$root.' does not exist');
+			continue;
+		}
+		//clone in root contents
+		dolog('install root '.$root);
+		$manifest = readManifest(ROOT.$root_path);
+		foreach($manifest as $mani_item)
+			run('cp -av '.ROOT.$root_path.$mani_item.' '.$dest.$root_path.$mani_item);
+	}
+	dolog('done installing roots');
 }
 
 //copy the leafs
 if(gfa($opts,'leafs')){
 	dolog('starting to install leafs');
 	foreach(explode(',',gfa($opts,'leafs')) as $leaf){
-		$leaf_path = ROOT.'/leaf/'.$leaf.'/';
+		$leaf_path = '/leaf/'.$leaf.'/';
 		//check if leaf exists
-		if(!is_dir($leaf_path)){
+		if(!is_dir(ROOT.$leaf_path)){
 			dolog('WARNING: leaf '.$leaf.' does not exist');
 			continue;
 		}
 		//clone in leaf contents
 		dolog('installing leaf '.$leaf);
-		$manifest = readManifest($leaf_path);
+		$manifest = readManifest(ROOT.$leaf_path);
 		foreach($manifest as $mani_item)
-			run('cp -av '.$leaf_path.$mani_item.' '.$dest);
+			run('cp -av '.ROOT.$leaf_path.$mani_item.' '.$dest.$leaf_path.$mani_item);
 	}
 	dolog('done installing leafs');
 }
